65fe29e78c49f136d24714abe2d38aa8bb696a19
Bruna Spaniol <bruna.spaniol@gmail.com>
2023-06-23 10:42:38 -0300

    configurando o express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("bruna")
+import express, { Request, Response } from "express";
+
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(3003, () => {
+  console.log("Servidor rodando na porta 3003");
+});
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("Pong!");
+});
